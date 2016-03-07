85dd21b518bd25998a31e16dc205ac23c41b622c
Peter Rupa <megatricycle@gmail.com>
2016-03-07 20:08:25 +0800

    Add test support for server

---
+++ models/sample.spec.js
@@ -0,0 +1,13 @@
+// this is a sample test file. All test files should end with .spec.js. It will automatically be tested on rebuild.
+
+import chai from 'chai';
+
+chai.should();
+
+describe('Test Component', () => {
+    it('should equal to 1', () => {
+        let x = 1;
+        
+        x.should.equal(1);
+    });
+});

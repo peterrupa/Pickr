4f035816c6c8eb3092359d7a893ac76f68bec0ca
aquinonikki <aquino.nm@gmail.com>
2016-03-09 11:37:22 +0800

    account creation

---
+++ controllers/accountController.js
@@ -0,0 +1,22 @@
+/*
+    Controller for the model "account".
+*/
+
+import express from 'express';
+let router  = express.Router();
+
+// be sure to import your model here
+import { Account } from '../models';
+
+export function insert(req, res) {
+    Account.create({
+	fname: req.body.fname,
+	mi: req.body.mi,
+	lname: req.body.lname,
+	username: req.body.username,
+	emailAddress: req.body.email,
+	password: req.body.password
+    }).then(function(account) {
+        res.send(account);
+    });
+}
---
+++ routes/account.js~
@@ -0,0 +1,7 @@
+import express from 'express';
+let router = express.Router();
+import * as accountController from '../controllers/accountController';
+
+router.post('/', accountController.insert);
+
+export default router;

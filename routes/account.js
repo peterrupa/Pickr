637a056a95096c231c4910d97db9f8caa5d9e4cb
PJHRobles <joshuahrobles@gmail.com>
2016-03-16 11:28:24 +0800

    Added functional login/logout routes

--- routes/account.js
+++ routes/account.js
@@ -2,6 +2,8 @@ import express from 'express';
 let router = express.Router();
 import * as accountController from '../controllers/accountController';
 
-router.post('/', accountController.insert);
+router.post('/createAccount', accountController.insert);
+router.post('/login', accountController.login);
+router.get('/logout', accountController.logout);
 
 export default router;

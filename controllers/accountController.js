bfb726d9d3a215194877a08de2340bdadc0b5351
lykamacusi <dgmacusi@up.edu.ph>
2016-03-16 11:36:36 +0800

    Updated routes, models and controllers

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -10,12 +10,12 @@ import { Account } from '../models';
 
 export function insert(req, res) {
     Account.create({
-	fname: req.body.fname,
-	mi: req.body.mi,
-	lname: req.body.lname,
-	username: req.body.username,
-	emailAddress: req.body.email,
-	password: req.body.password
+        fname: req.body.fname,
+        mi: req.body.mi,
+        lname: req.body.lname,
+        username: req.body.username,
+        emailAddress: req.body.email,
+        password: req.body.password
     }).then(function(account) {
         res.send(account);
     });
--- models/account.js
+++ models/account.js
@@ -1,11 +1,11 @@
 export default function (sequelize, DataTypes) {
     let Account = sequelize.define("Account", {
         Fname: DataTypes.STRING,
-	MI: DataTypes.STRING,
-	Lname: DataTypes.STRING,
-	Username: DataTypes.STRING,
-	EmailAddress: DataTypes.STRING,
-	Password: DataTypes.STRING
+        MI: DataTypes.STRING,
+        Lname: DataTypes.STRING,
+        Username: DataTypes.STRING,
+        EmailAddress: DataTypes.STRING,
+        Password: DataTypes.STRING
     });
 
     return Account;

842daa8f2c152c661afd098a8cd0beed20e3d9c2
PJHRobles <joshuahrobles@gmail.com>
2016-04-11 11:21:21 +0800

    fixed routing but session (redis store now used) saving still has issues

--- app.js
+++ app.js
@@ -5,25 +5,30 @@ import favicon from 'serve-favicon';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import redis from 'redis';
+import connectRedis from 'connect-redis';
 
 import student from './routes/student';
 import sample from './routes/sample';
 import account from './routes/account';
 let app = express();
+let client = redis.createClient();
+let redisStore = connectRedis(session);
+let store = new redisStore({host:'localhost', port:6379, client: client});
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+"/public"));
 
+client.on('connect', (err) => {
+    console.log('Redis now connected');
+    client.expire('*', 1000 * 60 * 60 * 5);
+});
 
-
+client.on('error', (err) => {
+    console.log('Error: ' + err);
+});
 
 app.set('view engine', 'ejs');
-
-app.use(session({
-    secret: 'PUT01SL0V3_PUT01SL1F3',
-    resave: false,
-    saveUninitialized: true
-}));
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname+"/public"));
@@ -34,12 +39,52 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(session({
+    secret: 'PUT01SL0V3_PUT01SL1F3',
+    store: store,
+    resave: false,
+    rolling: true,
+    saveUninitialized: false,
+    name: 'C0oK13_M0NZt3R',
+    cookie: {
+        path: '/',
+        httpOnly: false,
+        secure: false, // set "true" if https
+        maxAge: 1000 * 60 * 60 * 5, // 5 hours
+        domain: 'localhost'
+    }
+}));
+
 app.use('/api/student', student);
 app.use('/api/sample', sample);
 app.use('/api/account', account);
 
 // send routing to client
-app.get('*', (req, res) => {
+app.use('*', (req, res, next) => {
+
+    if (req.session && req.session.key) {
+        return next();
+    }
+    if (req.originalUrl in {'/register':'', '/#':'', '/':'', '/login':''}) {
+        res.sendFile(__dirname + '/src/index.html');
+    } else {
+        res.redirect('/login');
+    }
+
+},
+(req, res, next) => {
+
+    if (!(req.path in {'/register':'', '/#':'', '/':'', '/login':''})) {
+        return next();
+    }
+    if (req.originalUrl === '/class') {
+        return next();
+    } else {
+        res.redirect('/class');
+    }
+
+},
+(req, res, next) => {
     res.sendFile(__dirname + '/src/index.html');
 });
 
--- controllers/accountController.js
+++ controllers/accountController.js
@@ -10,17 +10,18 @@ import * as error from '../src/constants/ErrorTypes';
 import { Account } from '../models';
 
 exports.insert = (req, res) => {
-    
+
     if (!req.body.fname && !req.body.mi && !req.body.lname && !req.body.username
     && !req.body.email && !req.body.password) {
         res.status(error.INC_DATA.code).send({INC_DATA: error.INC_DATA.message});
     }
-    
+
     Account.findOne({
         where: {
             username: req.body.username
         }
-    }).then((user) => {
+    })
+    .then((user) => {
         if(user) {
             res.status(error.DUP_ENTRY.code).send({DUP_ENTRY: error.DUP_ENTRY.message});
         } else {
@@ -31,12 +32,14 @@ exports.insert = (req, res) => {
                 username:     req.body.username,
                 emailAddress: req.body.email,
                 password:     req.body.password
-            }).then((account) => {
+            })
+            .then((account) => {
                 res.status(200).send(account);
-            }).catch((err) => {
-                res.status(error.NO_RECORD_CREATED.code).send({NO_RECORD_CREATED: error.NO_RECORD_CREATED.message});    
-            });  
-        }   
+            })
+            .catch((err) => {
+                res.status(error.NO_RECORD_CREATED.code).send({NO_RECORD_CREATED: error.NO_RECORD_CREATED.message});
+            });
+        }
     }).catch((err) =>{
         res.status(error.SERVER_ERR.code).send({SERVER_ERR: error.SERVER_ERR.message});
     });
@@ -50,10 +53,12 @@ exports.login = (req, res) => {
         }
     })
     .then((user) => {
-        if(!user && !req.session.user) {
+
+        if(!user && !req.session.key) {
             res.status(error.INV_USER.code).send({INV_USER: error.INV_USER.message});
-        } 
-        else {
+
+        } else {
+
             Account.findOne({
                 where: {
                     username: req.body.username,
@@ -61,36 +66,39 @@ exports.login = (req, res) => {
                 }
             })
             .then((user) => {
-                if(!user && !req.session.user) {
+
+                if(!user && !req.session.key) {
                     res.status(error.INV_PASS.code).send({INV_PASS: error.INV_PASS.message})
-                } 
+                }
                 else {
-                    if (!req.session.user) {
-                        req.session.user = req.body.username;
-                        res.status(200).send({username:user.dataValues.Username, status:'logged in'});
-                    } 
-                    else {
+
+                    if (req.session && req.session.key) {
                         res.status(error.UNAUTH.code).send({UNAUTH: error.UNAUTH.message});
                     }
+                    else {
+                        req.session.key = user.dataValues.username;
+                        res.status(200).send(req.session);
+                    }
+
                 }
+
             })
             .catch((err) => {
+
                 res.status(error.LOG_FAIL.code).send({LOG_FAIL: error.LOG_FAIL.message});
+
             });
         }
     })
     .catch((err) => {
+
         res.status(error.LOG_FAIL.code).send({LOG_FAIL: error.LOG_FAIL.message});
+
     });
 
 }
 
 exports.logout = (req, res) => {
-    if (req.session.user) {
-        req.session.destroy();
-        res.status(200).send({status:'logged out'});
-    } else {
-        res.status(error.UNAUTH.code).send({UNAUTH: error.UNAUTH});
-    }
-    
+    req.session.destroy();
+    res.redirect('/login');
 }

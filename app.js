edbb92b0dea68b3576136755d99f7e68c6d0002c
keanpaderes <kean.paderes@gmail.com>
2016-05-03 17:02:19 +0800

    Converting to redis.

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ import favicon from 'serve-favicon';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
-import cors from 'cors';
-import uuid from 'uuid';
 import sequelize from './tools/sequelize';
-import store from './tools/store';
+
+import redis from 'redis';
+const client = redis.createClient();
+
+import connect from 'connect-redis';
+const redisStore = connect(session);
 
 import student from './routes/student';
 import sample from './routes/sample';
@@ -17,10 +20,21 @@ import activity from './routes/activity';
 import classRoute from './routes/class';
 import volunteer from './routes/volunteer';
 
-export var sessionId, sessionKey;
 let app = express();
-
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'PUT01SL0V3_PUT01SL1F3',
+    resave: false,
+    saveUninitialized: false,
+    store: new redisStore({
+        host: 'localhost',
+        port: 6379,
+        client: client,
+        ttl :  260
+    })
+}));
+
 app.use(express.static(__dirname+"/public"));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -28,33 +42,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({
-    origin: true,
-    methods: ['GET','POST','PUT','DELETE'],
-    allowedHeaders: ['Content-Type'],
-    credentials: true
-}));
 
-app.use(session({
-    secret: 'PUT01SL0V3_PUT01SL1F3',
-    store: store,
-    resave: false,
-    saveUninitialized: true,
-    genid: function(req){
-        return uuid.v4()
-    },
-    cookie: {
-        httpOnly: false,
-        secure: false, // set "true" if https
-        maxAge: 1000 * 60 * 60 * 5
-    }
-}));
-
-app.use((req, res, next) => {
-    sessionKey = req.session.key;
-    req.key = req.session.key;
-    next();
-});
 
 app.use('/api/student', student);
 app.use('/api/sample', sample);
@@ -71,10 +59,8 @@ app.get('/api/*', (req, res) => {
 
 // send routing to client
 app.use('*', (req, res, next) => {
-    sessionId = req.session.id;
-    console.log("This is the session id: " + sessionId);
-    // console.log("This is the session key: " + req.key);
-    if (req.session && req.session.key) {
+    console.log(req.session.id);
+    if (req.session.key) {
         return next();
     }
     if (req.originalUrl in {'/signup':'', '/register':'', '/#':'', '/':'', '/login':''}) {
@@ -99,7 +85,5 @@ app.use('*', (req, res, next) => {
 (req, res, next) => {
     res.sendFile(__dirname + '/src/index.html');
 });
-
-store.sync({force: false});
 
 export default app;
--- routes/account.js
+++ routes/account.js
@@ -2,9 +2,8 @@ import express from 'express';
 let router = express.Router();
 import * as accountController from '../controllers/accountController';
 
-router.get('/whoami', accountController.whoami);
 router.post('/createAccount', accountController.insert);
 router.post('/login', accountController.login);
-router.post('/logout', accountController.logout);
+router.delete('/logout', accountController.logout);
 
 export default router;

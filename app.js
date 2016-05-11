0d1269dbeaddb6c5436ad87e5cd6a6b9b76ea09d
Prince Aragones <princearagones@gmail.com>
2016-05-11 11:03:39 +0800

    added favicon

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use(session({
 
 app.use(express.static(__dirname+"/public"));
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
